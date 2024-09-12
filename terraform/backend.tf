21bf419db3f906a33452899ee06e1b0158506923
fred2030 <fredking19@yahoo.com>
2024-09-12 19:13:35 +0000

    made changes in the backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "new-springboot-app-s3-bucket"
     key            = "terraform_statefile"
     region         = "us-east-1"
-    dynamodb_table = "new-springboot-app-DynamoDB"
+    dynamodb_table = "springboot-app-project-DynamoDB"
   }
 }
